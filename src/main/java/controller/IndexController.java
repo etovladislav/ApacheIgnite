4f90004f85999494bfdc0df86fec55929f7bfae1
etovladislav <etovladislav@yandex.ru>
2016-11-18 23:55:56 +0300

    pidr

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,7 +1,12 @@
 package controller;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Created by mikl on 18.11.16.
@@ -10,7 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/")
-    public String getIndex(){
+    public String getIndex() {
+        Ignite ignite = Ignition.ignite();
+        final IgniteCache<Integer, String> cache = ignite.cache("cacheName");
+        for (int i = 0; i < 10; i++)
+            cache.put(i, Integer.toString(i));
+        for (int i = 0; i < 10; i++)
+            System.out.println("Got [key=" + i + ", val=" + cache.get(i) + ']');
+        cache.putIfAbsent(1, "1");
+        cache.replace(1, "1", "2");
+
         return "test";
+    }
+
+    @RequestMapping("/brat")
+    @ResponseStatus(HttpStatus.OK)
+    public void getBrat() {
+        Ignite ignite = Ignition.ignite();
+        final IgniteCache<Integer, String> cache = ignite.cache("cacheName");
+        for (int i = 0; i < 10; i++)
+            System.out.println("Got brat [key=" + i + ", val=" + cache.get(i) + ']');
     }
 }
