7b35d16e38a5b8ed1b0cb9b393ba59483d348c0d
etovladislav <etovladislav@yandex.ru>
2016-11-19 10:46:54 +0300

    backups

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -2,9 +2,13 @@ package controller;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCluster;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cluster.ClusterMetrics;
+import org.apache.ignite.cluster.ClusterNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -14,18 +18,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String getIndex() {
-        Ignite ignite = Ignition.start();
-        ignite = Ignition.ignite();
-        final IgniteCache<Integer, String> cache = ignite.cache("cacheName");
-        for (int i = 0; i < 10; i++)
-            cache.put(i, Integer.toString(i));
+    private static final Ignite ignite = Ignition.start("/Users/etovladislav/Desktop/apache-ignite-fabric-1.7.0-bin/examples/config/example-ignite2.xml");
+
+    @RequestMapping("/{pidr}")
+    public String getIndex(@PathVariable("pidr") String pidr) {
+        IgniteCluster cluster = ignite.cluster();
+        ClusterNode localNode = cluster.localNode();
+        ClusterMetrics metrics = localNode.metrics();
+        double cpuLoad = metrics.getCurrentCpuLoad();
+        long usedHeap = metrics.getHeapMemoryUsed();
+        int numberOfCores = metrics.getTotalCpus();
+        int activeJobs = metrics.getCurrentActiveJobs();
+        final IgniteCache<Integer, String> cache = ignite.getOrCreateCache("cacheName");
         for (int i = 0; i < 10; i++)
             System.out.println("Got [key=" + i + ", val=" + cache.get(i) + ']');
-        cache.putIfAbsent(1, "1");
-        cache.replace(1, "1", "2");
-
         return "test";
     }
 
